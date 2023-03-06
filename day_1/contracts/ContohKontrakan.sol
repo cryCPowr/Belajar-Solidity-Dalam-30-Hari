e3315953b0c1875b4feeaedc26ca525e8cd40a33
cryCPowr <farhanagilalmunzid@gmail.com>
2023-03-07 01:37:02 +0700

    Hari Pertama

--- day_1/contracts/ContohKontrakan.sol
+++ day_1/contracts/ContohKontrakan.sol
@@ -3,28 +3,50 @@
 pragma solidity ^0.8.18;
 
 contract SimulasiKontrakan {
-        uint256 public sisaKontrakan = 10;
-        string bisnis;
-        mapping (uint => Bisnis) public cekKontrakan;
-                    constructor() {
-                        bisnis = "Bisnis Kontrakan";
-                    }
-                    struct Bisnis{
-                        uint _unitKontrakan;
-                        string _namaPemilik;
-                    }
-        function bisnisApa() public view returns (string memory){
-            return bisnis;
-        }
-        function tambahPemilik(string memory _namaPemilik, uint _unitKontrakan) public {
-            require(_unitKontrakan <= sisaKontrakan, "Jumlah unit kontrakan yang akan ditempati melebihi jumlah sisa kontrakan yang tersedia.");
-            sisaKontrakan -= _unitKontrakan;
-            cekKontrakan[sisaKontrakan] = Bisnis(_unitKontrakan, _namaPemilik);
-        }
-            function tarikKontrakan(uint _unitKontrakan) public {
-            require(_unitKontrakan <= sisaKontrakan, "Jumlah unit kontrakan yang akan ditarik tidak dapat melebihi jumlah sisa kontrakan yang tersedia.");
-            require(sisaKontrakan + _unitKontrakan <= sisaKontrakan, "Jumlah unit kontrakan yang akan ditarik menyebabkan jumlah sisa kontrakan melebihi batas maksimum.");
-            delete cekKontrakan[_unitKontrakan];
-            sisaKontrakan += _unitKontrakan;
-}
+    address public juragan;
+    enum StatusKontrakan { Kosong, Terisi }
+    uint256 public sisaKontrakan = 10;
+    string bisnis;
+    mapping (uint => Bisnis) public daftarKontrakan;
+
+    constructor() {
+        bisnis = "Bisnis Kontrakan";
+        juragan = msg.sender;
+    }
+
+    modifier hanyaJuragan() {
+        require(msg.sender == juragan, "Hanya juragan yang berwenang disini");
+        _;
+    }
+
+    struct Bisnis {
+        uint _unitKontrakan;
+        string _namaPemilik;
+        StatusKontrakan _status;
+    }
+
+    function bisnisApa() public view returns (string memory) {
+        return bisnis;
+    }
+
+    function tambahPemilik(string memory _namaPemilik, uint _unitKontrakan) public hanyaJuragan {
+        require(_unitKontrakan <= sisaKontrakan, "Jumlah unit kontrakan yang akan ditempati melebihi jumlah sisa kontrakan yang tersedia.");
+        sisaKontrakan -= _unitKontrakan;
+        daftarKontrakan[sisaKontrakan] = Bisnis(_unitKontrakan, _namaPemilik, StatusKontrakan.Terisi);
+    }
+
+    function tarikKontrakan(uint _unitKontrakan) public hanyaJuragan {
+        require(_unitKontrakan <= sisaKontrakan, "Jumlah unit kontrakan yang akan ditarik tidak dapat melebihi jumlah sisa kontrakan yang tersedia.");
+        require(sisaKontrakan + _unitKontrakan <= 10, "Jumlah unit kontrakan yang akan ditarik menyebabkan jumlah sisa kontrakan melebihi batas maksimum.");
+        delete daftarKontrakan[sisaKontrakan + 1];
+        sisaKontrakan += _unitKontrakan;
+    }
+    
+    function cekKontrakan(uint _nomorKontrakan) public view returns (uint, string memory, string memory, StatusKontrakan) {
+        require(_nomorKontrakan >= 0 && _nomorKontrakan < 10, "Nomor kontrakan tidak valid.");
+        Bisnis storage kontrakan = daftarKontrakan[_nomorKontrakan];
+        require(kontrakan._status == StatusKontrakan.Terisi, "Kontrakan kosong.");
+        return (_nomorKontrakan, kontrakan._namaPemilik, bisnis, kontrakan._status);
+    }
+
 }
